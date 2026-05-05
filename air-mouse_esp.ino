8601ea5725bc0881192dc62f46ac324dd47c6c62
Ayush Srivastava <136251486+ayush1234s@users.noreply.github.com>
2026-05-06 01:52:02 +0530

    Air Mouse Code  / Circuit Diagram

---
+++ air-mouse_esp.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(21, 22);
+
+  Serial.println("Scanning...");
+}
+
+void loop() {
+  byte error, address;
+  int nDevices = 0;
+
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("Device found at 0x");
+      Serial.println(address, HEX);
+      nDevices++;
+    }
+  }
+
+  if (nDevices == 0)
+    Serial.println("No I2C devices found");
+
+  delay(2000);
+}
